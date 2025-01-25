efb047e5108c57dc6d89519522cf9ee162ea4a94
GudiVaraprasad <gudi.varaprasad@gmail.com>
2025-01-25 15:47:14 -0500

    ride service

--- gateway/app.js
+++ gateway/app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/user', expressproxy('http://localhost:3001'));
 app.use('/captain', expressproxy('http://localhost:3002'));
+app.use('/ride', expressproxy('http://localhost:3003'));
 
 app.listen(PORT, () => {
   console.log(`Gateway running on port ${PORT}`);
